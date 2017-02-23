fa074076da7a7f2de0674605090310be69fff40e
char256 <453276470@qq.com>
2017-02-23 15:35:54 +0800

    14 training data and some little update

--- poetry_utils.py
+++ poetry_utils.py
@@ -5,7 +5,7 @@ import cn_utils
 input_dim = 20
 max_word = 100
 
-def generate_feature(poetry):
+def _generate_feature(poetry):
     assert type(poetry.title[0]) == types.IntType
     feature = np.zeros(input_dim)
     feature[0:len(poetry.title)] = poetry.title
@@ -22,10 +22,18 @@ def generate_data(poetries, data, split = 'train'):
     captions = np.zeros((N,T))
     for i in xrange(N):
         pt = poetries[i]
-        feature = generate_feature(pt)
+        feature = _generate_feature(pt)
         features[i,:] = feature
         captions[i,:len(pt.content)] = pt.content
     
     data['%s_features' % split] = features
     data['%s_captions' % split] = captions
     return data
+
+def title_to_feature(title, word_to_idx):
+    feature = np.zeros((1,input_dim))
+    assert len(title) <= 60, 'the length of title should not be longer than 20 words!'
+
+    caption = np.array(cn_utils.encodeChinese(title, word_to_idx))
+    feature[0, :caption.shape[0]] = caption
+    return feature
